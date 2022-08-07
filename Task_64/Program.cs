6ad254a0734b1311a0089c5addb04e8771f0ea3b
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-08-07 16:55:24 +0300

    Добавлены решения в задачи

---
+++ Task_64/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8"" */
+
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers (int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + ", " + PrintNumbers (start + 1, end));
+}
+
+Console.WriteLine(PrintNumbers (m, n));
---
+++ Task_68/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9 */
+
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman (int m, int n)
+{
+    if (m == 0) 
+    return n + 1;
+    else if (m > 0 && n == 0)    
+    return Akkerman (m - 1, 1);
+    else return Akkerman (m - 1, Akkerman (m, n - 1));
+}
+
+Console.WriteLine(Akkerman (m, n));
